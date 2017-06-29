125f2fab62f0986b580ddc8a50c6d0d9866508d0
amalakamala <amalakamala.a@gmail.com>
2017-06-29 10:52:00 -0400

    localStore

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,6 @@
+//Contado id
+var cont = 0;
+
 $(document).ready(function(){
 
 	$('#hacer').html(localStorage.getItem('listItems'));
@@ -5,47 +8,51 @@ $(document).ready(function(){
 
 	$('body').keyup(function(e) {
 		
-
+		//Al apretar enter se crea la tarea
 	    if(e.keyCode == 13) {
-	    	var tarea = $("#tarea").val();
+	    	var tarea = $("#tarea").val(); //Id desde html
 
 	        if(tarea == ""){
-				alert("Debes escribir una tarea");
+				alert("Debes escribir una tarea"); //input vacia da alert
 			}else{
-				$("#hacer").append('<div class="padre"><input type="checkbox" id="tarea-nueva" class="la-tarea" /><label for="tarea-nueva">' + tarea + '</label><a class="waves-effect waves-light right btn sacar">remove</a><br><div>');
+				$("#hacer").append('<div class="padre"><input type="checkbox" id="tarea-nueva-'+cont+'" class="la-tarea" /><label for="tarea-nueva-'+cont+'">' + tarea + '</label><a class="waves-effect waves-light right btn sacar">remove</a><br><div>');
 				localStorage.setItem('listItems', $('#hacer').html());
 	        	$("#tarea").val("");	
+	        	cont++;
+	        	console.log(cont);
 			}	                
 	    }
 
-	    // console.log($(".padre").toArray());
-
-	    $('.sacar').click(function(){
-			$(this).parent().remove();
-			localStorage.setItem('listItems', $('#hacer').html());
-			localStorage.setItem('listItemsDos', $('#hechas').html());
-		})
-
-	    /*
-	    $('.padre').each(function( index ) {
-  			console.log( index + ": " + $(this).text());
-		});
-		*/
-
-
-
-		$('[type="checkbox"]').click(function(){
-
-			$($(this).parent()).appendTo("#hechas");
-			$(this).remove();
-			$("#hechas label").attr("class","label-dos");
-
-			//$("#hechas").append($(this).parent());
-			localStorage.setItem('listItemsDos', $('#hechas').html());
-
-		})
-
+	    sacar();
+	    poner();
 
 	});
 
+
+function sacar(){
+	$('.sacar').click(function(){
+		$(this).parent().remove();
+		localStorage.removeItem('listItems');
+		localStorage.removeItem('listItemsDos');
+	})
+}
+
+function poner(){
+	$('[type="checkbox"]').click(function(){
+		$($(this).parent()).appendTo("#hechas");
+		$(this).remove();
+		$("#hechas label").attr("class","label-dos");
+
+		//$("#hechas").append($(this).parent());
+		localStorage.setItem('listItemsDos', $('#hechas').html());
+		localStorage.getItem('listItems', $('#hacer').html());
+
+	})	
+}
+
+sacar();
+poner();
+
 });
+
+
