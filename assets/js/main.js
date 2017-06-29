7747a2b8e559759aa33fc36cbdf3e9b157c048a6
amalakamala <amalakamala.a@gmail.com>
2017-06-29 11:39:39 -0400

    comentarios

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,8 @@ var cont = 0;
 
 $(document).ready(function(){
 
-	$('#hacer').html(localStorage.getItem('listItems')); //Local store para las tareas por hacer
-	$('#hechas').html(localStorage.getItem('listItemsDos')); //Local store para las tareas hechas
+	$('#hacer').html(localStorage.getItem('listItems')); //LocalStorage para las tareas por hacer
+	$('#hechas').html(localStorage.getItem('listItemsDos')); //LocalStorage para las tareas hechas
 
 
 	//Para crear la nueva tarea
@@ -18,7 +18,7 @@ $(document).ready(function(){
 				alert("Debes escribir una tarea"); //input vacia da alert
 			}else{
 				$("#hacer").append('<div class="padre"><input type="checkbox" id="tarea-nueva-'+cont+'" class="la-tarea" /><label for="tarea-nueva-'+cont+'">' + tarea + '</label><a class="waves-effect waves-light right btn sacar">remove</a><br><div>');
-				localStorage.setItem('listItems', $('#hacer').html());
+				localStorage.setItem('listItems', $('#hacer').html()); //La tarea creada se guardara en localStorage
 	        	$("#tarea").val("");	
 	        	cont++;
 			}	                
@@ -38,15 +38,17 @@ function sacar(){
 	})
 }
 
+
+// function cambiar de estado las tareas
 function poner(){
 	$('[type="checkbox"]').click(function(){
 		$($(this).parent()).appendTo("#hechas");
-		$(this).remove();
-		$("#hechas label").attr("class","label-dos");
+		$(this).remove(); //saco el checbox porque la tarea ya esta hecha
+		$("#hechas label").attr("class","label-dos"); 
 
 		//$("#hechas").append($(this).parent());
-		localStorage.setItem('listItemsDos', $('#hechas').html());
-		localStorage.getItem('listItems', $('#hacer').html());
+		localStorage.setItem('listItemsDos', $('#hechas').html()); //Tareas pasan a hechas
+		localStorage.getItem('listItems', $('#hacer').html()); //Tareas sacadas de hacer
 
 	})	
 }
